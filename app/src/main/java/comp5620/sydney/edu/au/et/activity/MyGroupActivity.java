02b26c002659cc7ea0dbca55b10f97840af8b2c1
PAN YI <panyi105@gmail.com>
2018-10-25 00:19:04 +1100

    post UI

---
+++ app/src/main/java/comp5620/sydney/edu/au/et/activity/MyGroupActivity.java
@@ -0,0 +1,15 @@
+package comp5620.sydney.edu.au.et.activity;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import comp5620.sydney.edu.au.et.R;
+
+public class MyGroupActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_my_group);
+    }
+}
---
+++ app/src/main/java/comp5620/sydney/edu/au/et/activity/ViewPostActivity.java
@@ -0,0 +1,16 @@
+package comp5620.sydney.edu.au.et.activity;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+import comp5620.sydney.edu.au.et.R;
+
+public class ViewPostActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_view_post);
+
+    }
+}
